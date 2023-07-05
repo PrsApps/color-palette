06d27acc4decefe44e0a3069df76f3e35ec3bab6
paulo <paulorscontato@gmail.com>
2023-07-04 21:44:37 -0300

    limpeza no codigo

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// const panel = document.querySelector('.panel__color')
-// const panelInput = document.querySelector('.panel__input')
-// const panelButton = document.querySelector('.panel__button')
 
-// panelButton.addEventListener('click', function() {
-
-//     const color = panelInput.value
-
-//     console.log(color)
-//         if (color !=='') {
-//             panel.style.backgroundColor = color
-//         }
-// })
-
-const panel = document.querySelectorAll(".panel__color");
-const panelInput = document.querySelectorAll("input");
-const panelButton = document.querySelectorAll(".panel__button");
-const group = document.querySelectorAll(".group");
 
 function change() {
   const btn = event.target;
